337af65f8f3ec638f9acdcdd74fc594fd2c446ed
jdelphy <jdelphy@gmail.com>
2021-03-22 13:06:23 -0400

    add directory

--- 3-user-settings.sh
+++ 3-user-settings.sh
@@ -6,6 +6,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 CLEAR='\033[0m'
 
+mkdir -p ~/Downloads/hwr-temp && cd ~/Downloads/hwr-temp/
 #tar -cpvf /home/oem/Desktop/configs.tar /home/oem/.cinnamon/configs/
 wget -O ~/Downloads/hwr-temp/configs.tar https://github.com/auromak/hwr-post-install/raw/main/configs.tar
 sudo tar -xpf ~/Downloads/hwr-temp/configs.tar -C /

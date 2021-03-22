4e142ed9924295c2578d2b1b0d0911ace8659230
jdelphy <jdelphy@gmail.com>
2021-03-22 18:26:20 -0400

    changed settings/dconf order

--- 3-user-settings.sh
+++ 3-user-settings.sh
@@ -7,12 +7,15 @@ GREEN='\033[0;32m'
 CLEAR='\033[0m'
 
 mkdir -p ~/Downloads/hwr-temp && cd ~/Downloads/hwr-temp/
-#tar -cmvf /home/oem/Desktop/configs.tar /home/oem/.cinnamon/configs/
-wget -O ~/Downloads/hwr-temp/configs.tar https://github.com/auromak/hwr-post-install/raw/main/2021-03-22-1654-backup.tar
-sudo tar -xmvf ~/Downloads/hwr-temp/configs.tar -C ~/
 
+# Load defaults into dconf
 wget -O ~/Downloads/hwr-temp/configured-dconf-backup https://github.com/auromak/hwr-post-install/raw/main/configured-dconf-backup
 dconf load /org/cinnamon/ < configured-dconf-backup
+
+# Load settings
+#tar -cmvf /home/oem/Desktop/configs.tar /home/oem/.cinnamon/configs/ # Deprecated, currently using Backup Tool in Mint
+wget -O ~/Downloads/hwr-temp/configs.tar https://github.com/auromak/hwr-post-install/raw/main/2021-03-22-1654-backup.tar
+sudo tar -xmvf ~/Downloads/hwr-temp/configs.tar -C ~/
 # -xpf
 
 echo ""

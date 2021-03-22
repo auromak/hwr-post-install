7d69ec8342ea92a6f0b2088f4eb82b597776535e
jdelphy <jdelphy@gmail.com>
2021-03-21 23:56:13 -0400

    made automatic

--- 0-primary.sh
+++ 0-primary.sh
@@ -6,12 +6,14 @@
 ###############################################################################################
 
 wget -qO- https://raw.githubusercontent.com/auromak/hwr-post-install/main/1-zoho-install.sh | bash
+sleep 3
+wget -qO- https://raw.githubusercontent.com/auromak/hwr-post-install/main/2-user-theme.sh | bash
 
-while true; do
-    read -p "Would you like to apply changes to current user? " yn
-    case $yn in
-        [Yy]* ) wget -qO- https://raw.githubusercontent.com/auromak/hwr-post-install/main/2-user-theme.sh | bash; break;;
-        [Nn]* ) echo -e "Ok then, maybe next time.\n"; break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+# while true; do
+#     read -p "Would you like to apply changes to current user? " yn
+#     case $yn in
+#         [Yy]* ) wget -qO- https://raw.githubusercontent.com/auromak/hwr-post-install/main/2-user-theme.sh | bash; break;;
+#         [Nn]* ) echo -e "Ok then, maybe next time.\n"; break;;
+#         * ) echo "Please answer yes or no.";;
+#     esac
+# done

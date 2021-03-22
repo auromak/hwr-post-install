92487729243da14b19cc2e0214113e77034d5c9d
jdelphy <jdelphy@gmail.com>
2021-03-22 17:49:13 -0400

    loading dconf presets

--- 3-user-settings.sh
+++ 3-user-settings.sh
@@ -10,6 +10,9 @@ mkdir -p ~/Downloads/hwr-temp && cd ~/Downloads/hwr-temp/
 #tar -cmvf /home/oem/Desktop/configs.tar /home/oem/.cinnamon/configs/
 wget -O ~/Downloads/hwr-temp/configs.tar https://github.com/auromak/hwr-post-install/raw/main/2021-03-22-1654-backup.tar
 sudo tar -xmvf ~/Downloads/hwr-temp/configs.tar -C ~/
+
+wget -O ~/Downloads/hwr-temp/backup_of_my_cinnamon_settings https://github.com/auromak/hwr-post-install/raw/main/backup_of_my_cinnamon_settings
+dconf load /org/cinnamon/ < backup_of_my_cinnamon_settings
 # -xpf
 
 echo ""

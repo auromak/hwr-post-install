e8a6e32ace5f230e708108114c66cff80dc77e03
Justin <jdelphy@gmail.com>
2021-03-22 23:15:43 -0400

    colors are fun

--- 0-hwr-initial-config.sh
+++ 0-hwr-initial-config.sh
@@ -14,6 +14,7 @@ hwr_dir=/usr/share/hwrescue/
 temp_dir=~/Downloads/hwr-temp/
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+BOLD="\033[1m"
 CLEAR='\033[0m'
 
 ## make sure you are sudo:
@@ -23,7 +24,6 @@ else
 	echo ""
 fi
 
-echo ""
 echo "Installing core software:"
 echo ""
 
@@ -36,7 +36,7 @@ mkdir -p "$temp_dir" && cd "$temp_dir"
 # Check for and install Papirus icons and misc applets (Skips all if Papirus is found)
 if [ -d "$icons_dir"Papirus/ ]
 then
-	echo -e "\nPapirus is ${GREEN}already installed${CLEAR}. Skipping..."
+	echo -e "\nPapirus is ${GREEN}${BOLD}already installed${CLEAR}. Skipping..."
 else
 	# Install Papirus Icons
 	wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/install.sh | bash
@@ -45,14 +45,14 @@ fi
 # Install Applets
 if [ -d "$applets_dir"Cinnamenu@json/ ]
 then
-	echo -e "\nCinnamenu is ${GREEN}already installed${CLEAR}. Skipping..."
+	echo -e "\nCinnamenu is ${GREEN}${BOLD}already installed${CLEAR}. Skipping..."
 else
 	wget https://cinnamon-spices.linuxmint.com/files/applets/Cinnamenu@json.zip && sudo unzip Cinnamenu@json.zip -d "$applets_dir"
 fi
 
 if [ -d "$applets_dir"weather@mockturtl/ ]
 then
-	echo -e "\nWeather is ${GREEN}already installed${CLEAR}. Skipping..."
+	echo -e "\nWeather is ${GREEN}${BOLD}already installed${CLEAR}. Skipping..."
 else
 	wget https://cinnamon-spices.linuxmint.com/files/applets/weather@mockturtl.zip && sudo unzip weather@mockturtl.zip -d "$applets_dir"
 fi
@@ -60,7 +60,7 @@ fi
 # Install Extensions
 if [ -d "$extensions_dir"watermark@germanfr/ ]
 then
-	echo -e "\nWatermark is ${GREEN}already installed${CLEAR}. Skipping..."
+	echo -e "\nWatermark is ${GREEN}${BOLD}already installed${CLEAR}. Skipping..."
 else
 	wget https://cinnamon-spices.linuxmint.com/files/extensions/watermark@germanfr.zip && sudo unzip watermark@germanfr.zip -d "$extensions_dir"
 	sudo wget -O "$hwr_dir"hwr-watermark.svg https://raw.githubusercontent.com/auromak/hwr-post-install/main/HWR7-watermark-white-optimized-FINAL.svg
@@ -70,7 +70,7 @@ fi
 dpkg -l zohoassist > /dev/null 2>&1
 INSTALLED=$?
 if [ $INSTALLED == '0' ]; then
-	echo -e "\nZohoAssist is ${GREEN}already installed${CLEAR}. Skipping..."
+	echo -e "\nZohoAssist is ${GREEN}${BOLD}already installed${CLEAR}. Skipping..."
 else
 	wget "https://assist.zoho.com/api/v2/install_urs?type=2&encapiKey=wSsVRa12%2BhehD%2F0uyTCsIrxrnQxUBluiFxx631Wk4nX5GqjE88c9xEKdAFOnGfkWFzJtQDNBpr14zUpU1TMGjtwtmAsAWSiF9mqRe1U4J3x1pL7mlDPPW21dkxOILIoLwwxtkg%3D%3D&app=linux&version=64bit&isDebian=true" -O zohoassist_1.0.0.1.deb
 	sudo dpkg -i zohoassist_1.0.0.1.deb
@@ -81,7 +81,7 @@ fi
 # ...
 
 echo ""
-echo -e "${GREEN}Finished installing${CLEAR} software and extensions"
+echo -e "${GREEN}${BOLD}Finished installing${CLEAR} software and extensions"
 echo ""
 echo -e "Applying settings to current user:"
 echo ""
@@ -98,11 +98,11 @@ rm -rf ~/.mozilla ~/.cinnamon
 tar -xmvf "$temp_dir"/configs.tar -C ~/
 
 echo ""
-echo -e "User ${GREEN}settings applied${CLEAR}"
+echo -e "User ${GREEN}${BOLD}settings applied${CLEAR}"
 echo ""
 # Cleanup
 echo "Cleaning up temporary files..."
 rm -r "$temp_dir"
 echo ""
-echo "All done!"
+echo -e "${BOLD}All done!${CLEAR}"
 echo ""
